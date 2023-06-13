74a5ea72165353af3a612edd39adc6ae2a66945a
Antonello Laurino <antonello.laurino@gmail.com>
2023-06-13 16:04:04 +0200

    Implemented next and previous UI buttons for photobook navigation

--- Assets/_MyAssets/Scripts/BookController.cs
+++ Assets/_MyAssets/Scripts/BookController.cs
@@ -2,25 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Animations;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BookController : MonoBehaviour
 {
     [SerializeField]
     private List<Material> photos = new List<Material>(); //list of materials; each material has a texture of a single photo
-    public int currentPage = 0; //NB: each page has 2 photos
+    private int currentPage = 0; //NB: each page has 2 photos
 
     [Header("Pages reference")]
-    public GameObject firstPage_1;
-    public GameObject centralPage_2;
-    public GameObject centralPage_3;
-    public GameObject lastPage_4;
+    [SerializeField]
+    private GameObject firstPage_1;
+    [SerializeField]
+    private GameObject centralPage_2;
+    [SerializeField]
+    private GameObject centralPage_3;
+    [SerializeField]
+    private GameObject lastPage_4;
+
+    [Header("UI Buttons reference")]
+    [SerializeField]
+    private Button nextButton;
+    [SerializeField]
+    private Button previousButton;
 
     [SerializeField]
     private Animator bookAnimatorController;
-
-    private const string PREVIOUS_PAGE_ANIM = "PreviousPage";
-    private const string NEXT_PAGE_ANIM = "NextPage";
-    private const string IDLE_ANIM = "Idle";
 
     private void Update()
     {
@@ -34,58 +41,86 @@ public class BookController : MonoBehaviour
         }
     }
 
+    private bool isLastPhoto()
+    {
+        return currentPage >= (photos.Count / 2 - 1);
+    }
+
+    private bool isFirstPhoto()
+    {
+        return currentPage == 0;
+    }
+
     public void NextPage()
     {
-        if (currentPage >= (photos.Count/2 - 1)) {
-            Debug.Log("Last photo");
-            return;
-        }
-        if (!bookAnimatorController.GetCurrentAnimatorStateInfo(0).IsName(IDLE_ANIM))
+        if (isLastPhoto()) { return; }
+        if (!bookAnimatorController.GetCurrentAnimatorStateInfo(0).IsName(Constant.IDLE_ANIM))
         {
             //one animation is already playing
             return;
         }
 
+        //set the indexes
         int indexActualFirstPhoto = currentPage * 2;
         int indexActualSecondPhoto = indexActualFirstPhoto + 1;
         int indexNewFirstPhoto = indexActualSecondPhoto + 1;
         int indexNewSecondPhoto = indexNewFirstPhoto + 1;
-        currentPage++;
-
+        
+        //change materials of each page:
+        //firstPage_1 and centralPage_2 are the pages visible when the animation of page change starts
+        //centralPage_3 and lastPage_4 are the pages visible after the animation of page change
         firstPage_1.GetComponent<MeshRenderer>().material = photos[indexActualFirstPhoto];
         centralPage_2.GetComponent<MeshRenderer>().material = photos[indexActualSecondPhoto];
         centralPage_3.GetComponent<MeshRenderer>().material = photos[indexNewFirstPhoto];
         lastPage_4.GetComponent<MeshRenderer>().material = photos[indexNewSecondPhoto];
 
         //Play animation
-        bookAnimatorController.Play(NEXT_PAGE_ANIM);
+        bookAnimatorController.Play(Constant.NEXT_PAGE_ANIM);
+
+        //update index
+        currentPage++;
+
+        //if, after updating the index, we are at last photo, deactivate next button
+        //or, if we are at first photo, deactivate previous button
+        //the check is on both buttons in order to reactivate them after a previous deactivation
+        nextButton.interactable = !isLastPhoto();
+        previousButton.interactable = !isFirstPhoto();
     }
 
     public void PreviousPage()
     {
-        if (currentPage == 0)
-        {
-            Debug.Log("First photo");
-            return;
-        }
-        if (!bookAnimatorController.GetCurrentAnimatorStateInfo(0).IsName(IDLE_ANIM))
+        if (isFirstPhoto()) { return; }
+        if (!bookAnimatorController.GetCurrentAnimatorStateInfo(0).IsName(Constant.IDLE_ANIM))
         {
             //one animation is already playing
             return;
         }
 
+        //set the indexes
         int indexActualFirstPhoto = currentPage * 2;
         int indexActualSecondPhoto = indexActualFirstPhoto + 1;
         int indexNewFirstPhoto = indexActualFirstPhoto - 1;
         int indexNewSecondPhoto = indexNewFirstPhoto - 1;
-        currentPage--;
+        
 
+        //change materials of each page:
+        //firstPage_1 and centralPage_2 are the pages visible when the animation of page change starts
+        //centralPage_3 and lastPage_4 are the pages visible after the animation of page change
         firstPage_1.GetComponent<MeshRenderer>().material = photos[indexNewSecondPhoto];
         centralPage_2.GetComponent<MeshRenderer>().material = photos[indexNewFirstPhoto];
         centralPage_3.GetComponent<MeshRenderer>().material = photos[indexActualFirstPhoto];
         lastPage_4.GetComponent<MeshRenderer>().material = photos[indexActualSecondPhoto];
 
         //Play animation
-        bookAnimatorController.Play(PREVIOUS_PAGE_ANIM);      
+        bookAnimatorController.Play(Constant.PREVIOUS_PAGE_ANIM);
+
+        //update index
+        currentPage--;
+
+        //if, after updating the index, we are at first photo, deactivate previous button
+        //or, if we are at last photo, deactivate next button
+        //the check is on both buttons in order to reactivate them after a previous deactivation
+        previousButton.interactable = !isFirstPhoto();
+        nextButton.interactable = !isLastPhoto();
     }
 }
--- Assets/_MyAssets/Scripts/Constant.cs
+++ Assets/_MyAssets/Scripts/Constant.cs
@@ -4,6 +4,9 @@ public class Constant
     public const float HIGHLIGHT_GOAL_TIME = 1f;
     public const string PLAY_GAME = "PlayGame";
     public const string STOP_GAME = "StopGame";
+    public const string PREVIOUS_PAGE_ANIM = "PreviousPage";
+    public const string NEXT_PAGE_ANIM = "NextPage";
+    public const string IDLE_ANIM = "Idle";
 
     //TAGS
     public const string PLAYER_TAG = "Player";
