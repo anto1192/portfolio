49746dd8a4ae5948501a644caaf63867c15a4f57
Antonello Laurino <antonello.laurino@gmail.com>
2023-06-13 23:37:40 +0200

    Added video description UI panel and logic

--- Assets/_MyAssets/Scripts/VideoMonitorController.cs
+++ Assets/_MyAssets/Scripts/VideoMonitorController.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Video;
 
 [RequireComponent(typeof(VideoPlayer))]
 public class VideoMonitorController : MonoBehaviour
 {
+    [Header("List of video clips and descriptions")]
     [SerializeField]
     private List<VideoClip> clipList = new List<VideoClip>();
+    [SerializeField]
+    private List<string> clipDescriptionList = new List<string>();
+
+    [Header("Reference to description text")]
+    [SerializeField]
+    private TextMeshProUGUI descriptionText;
 
     private VideoPlayer player;
 
@@ -19,6 +27,7 @@ public class VideoMonitorController : MonoBehaviour
     public void PlayVideo(int index)
     {
         player.clip = clipList[index];
+        descriptionText.text = clipDescriptionList[index].Replace("\\n", "\n");
         player.Play();
     }
 }
